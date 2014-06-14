34c0af05f38c545bef4e736d71a61402d48ad615
Christian Tschoepe <tschoepe.christian@gmail.com>
2014-06-14 11:59:52 -0500

    Fix route

--- config/routes/groups.rb
+++ config/routes/groups.rb
@@ -1,7 +1,7 @@
 Eds::Application.routes.draw do
   resources :groups do 
     resources :stories do
-      post 'scene', action: 'story_scene'
+      post '/:id/scene', action: 'story_scene'
     end
   end
 end
