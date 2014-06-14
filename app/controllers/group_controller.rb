98d1a93ea0dcbdcbf4321076940bcf34fbebd5b4
Christian Tschoepe <tschoepe.christian@gmail.com>
2014-06-14 03:47:34 -0500

    ADded stuff for server for stories and for groups :)

--- app/controllers/group_controller.rb
+++ app/controllers/group_controller.rb
@@ -4,19 +4,32 @@ class GroupController < ApplicationController
     only: [:update, :create]
   
   def show
-    
+    @group_stories = @group.stories
   end
 
   def update
     
   end
 
+  def new
+
+  end
+
   def create
-    
+    group_hash = params.only('name', 'description')
+    g = Group.create(group_hash) 
+
+    g.users << @users
+    p.save!
   end
 
   def destroy
     
+  end
+
+  private
+  def get_group
+    @group = Group.get params[:id]
   end
 
 end
--- app/controllers/story_controller.rb
+++ app/controllers/story_controller.rb
@@ -4,8 +4,11 @@ class StoryController < ApplicationController
     only: [:update, :create]
   
   def show
-    @story = Story.get params[:id]
     @story_scenes = @story.story_scenes
+  end
+
+  def new
+
   end
 
   def update
@@ -14,23 +17,45 @@ class StoryController < ApplicationController
 
   def create
     story_hash = params.only('name', 'description')
-    rules_hash = params[:rules].only('')
+    rules_hash = params[:rules].only(
+      'min_chars',      'max_chars', 
+      'min_words',      'max_words',
+      'story_time_out', 'scene_time_out'
+    )
+
+    story = Story.create(story_hash)
+    story.rules = Rules.new(rules_hash)
+    story.save!
 
   end
 
   #POST to create a new scene.
   def story_scene
     scene_hash = params.only('body')
-    StoryScene.create(scene_hash)
+    
+    scene = StoryScene.new(scene_hash)
+
+    body = scene.body
+
+    rules = @story.rules
+
+    unless body.length < rules.min_chars and body.length > rules.max_chars 
+      body.words.size < rules.min_words and body.words.size > rules.max_words
+
+      @story.story_scenes << scene
+      @story.save!
+    else
+      flash[:alert] = {}
+    end
   end
 
   def destroy
-
+    @story.destroy!
   end
 
   private
   def get_story
-
+    @story = Story.get params[:id]
   end
 
 end
